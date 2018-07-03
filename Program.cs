534aec0d6b88bc1ff76dc6445cbdeee2ddfaf926
Matthias Reitinger <reima@edenprime.de>
2018-07-03 20:34:00 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorStandalone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
