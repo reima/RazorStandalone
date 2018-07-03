149bf2de9204ba81111897a5891ea3b0da2eefe3
Matthias Reitinger <reima@edenprime.de>
2018-07-03 21:21:58 +0200

    Add repro code.

--- Program.cs
+++ Program.cs
@@ -1,15 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.Language;
 
 namespace RazorStandalone
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            var razorEngine = RazorProjectEngine.Create(
+                RazorConfiguration.Default,
+                RazorProjectFileSystem.Create("Views"));
+
+            var item = razorEngine.FileSystem.GetItem("Test.cshtml");
+            var cSharpDocument = razorEngine.Process(item).GetCSharpDocument();
+
+            Console.WriteLine(cSharpDocument.GeneratedCode);
         }
     }
 }
