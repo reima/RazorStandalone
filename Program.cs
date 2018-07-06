8ddc60e3699f673379c647a24f622e6fba899fc3
Matthias Reitinger <reima@edenprime.de>
2018-07-07 00:19:23 +0200

    Added workaround.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.AspNetCore.Razor.Language;
+using Microsoft.AspNetCore.Razor.Language.CodeGeneration;
 
 namespace RazorStandalone
 {
@@ -9,12 +10,78 @@ namespace RazorStandalone
         {
             var razorEngine = RazorProjectEngine.Create(
                 RazorConfiguration.Default,
-                RazorProjectFileSystem.Create("Views"));
+                RazorProjectFileSystem.Create("Views"),
+                builder =>
+                {
+                    // Insert a new phase just before the code gen phase
+                    // (hopefully).
+                    builder.Phases.Insert(
+                        builder.Phases.Count - 1,
+                        new SetTemplateTypePhase("MyTemplateType"));
+                });
 
             var item = razorEngine.FileSystem.GetItem("Test.cshtml");
             var cSharpDocument = razorEngine.Process(item).GetCSharpDocument();
 
             Console.WriteLine(cSharpDocument.GeneratedCode);
+        }
+    }
+
+    internal class SetTemplateTypePhase : RazorEnginePhaseBase
+    {
+        private readonly string _templateTypeName;
+
+        public SetTemplateTypePhase(string templateTypeName)
+        {
+            _templateTypeName = templateTypeName;
+        }
+
+        protected override void ExecuteCore(RazorCodeDocument codeDocument)
+        {
+            var documentNode = codeDocument.GetDocumentIntermediateNode();
+            ThrowForMissingDocumentDependency(documentNode);
+
+            // Decorate the current CodeTarget to set the custom template type.
+            documentNode.Target =
+                new SetTemplateTypeCodeTargetDecorator(
+                    documentNode.Target,
+                    _templateTypeName);
+        }
+    }
+
+    internal class SetTemplateTypeCodeTargetDecorator : CodeTarget
+    {
+        private readonly CodeTarget _target;
+        private readonly string _templateTypeName;
+
+        public SetTemplateTypeCodeTargetDecorator(
+            CodeTarget target,
+            string templateTypeName)
+        {
+            _target = target;
+            _templateTypeName = templateTypeName;
+        }
+
+        public override IntermediateNodeWriter CreateNodeWriter()
+        {
+            var nodeWriter = _target.CreateNodeWriter();
+            if (nodeWriter is RuntimeNodeWriter runtimeNodeWriter)
+            {
+                // Set the custom template type.
+                runtimeNodeWriter.TemplateTypeName = _templateTypeName;
+            }
+
+            return nodeWriter;
+        }
+
+        public override TExtension GetExtension<TExtension>()
+        {
+            return _target.GetExtension<TExtension>();
+        }
+
+        public override bool HasExtension<TExtension>()
+        {
+            return _target.HasExtension<TExtension>();
         }
     }
 }
